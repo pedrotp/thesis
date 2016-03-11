f7d02b0ec8ab32a59561f4d424211f240da8efb7
Derek Huang <derekh919@gmail.com>
2016-03-10 19:28:40 -0500

    (chore) Added empty helpers.js file and adjusted pseudocode for GET error handler

--- server/routes.js
+++ server/routes.js
@@ -3,11 +3,19 @@ var helpers = require('./helpers');
 var routes = [
   {
     path: '/habits',
+    get: function (req, res) {
+      // query db for user's habits
+      // on success
+        // res.json(data);
+      // on Error
+        // console.error(err);
+        // res.statusCode(500);
+    },
     post: function(req, res) {
       var habit = req.body;
       helpers.addHabit(habit, function(data) {
             res.status(201).send(data);
-          }, 
+          },
           function(error) {
             console.error(error);
             res.sendStatus(400);
