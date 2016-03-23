8cc29a1666b52fd60e87b002d862a7cdedaa04d1
pedrotp <pedro@pedrotp.com>
2016-03-23 16:26:39 -0400

    (fix) fixes formatting for done / not done

--- app/components/Inbox.js
+++ app/components/Inbox.js
@@ -32,18 +32,16 @@ function Inbox (props) {
 
   return (
     <View style={styles.inboxitem}>
-      <Swipeout autoClose={true} right={swipeButtons} backgroundColor={'#EDBE40'}>
+      <Swipeout autoClose={true} right={swipeButtons} backgroundColor={'#fcfcfc'}>
         <View style={styles.swipe}>
           <View>
             <Text style={styles.habit}>{props.habit.action} every {props.habit.frequency}</Text>
             <Text style={styles.count}>You've done this {props.habit.instanceCount} time(s).</Text>
-            <Text style={ done ? styles.done : styles.notdone }>
-            { done ? 'DONE TODAY' : 'NOT DONE TODAY' }</Text>
           </View>
           <Image 
-              source={ done ? {uri: 'http://localhost:3000/assets/done2.png'} : {uri: 'http://localhost:3000/assets/not_done2.png'} }
-              style={ done ? styles.doneimg : styles.notdoneimg }
-            />
+              source={ done ? {uri: 'http://localhost:3000/assets/done_green.png'} : {uri: 'http://localhost:3000/assets/done_gray.png'} }
+              style={ styles.img }
+          />
         </View>
       </Swipeout>
     </View>
@@ -64,35 +62,18 @@ var styles = StyleSheet.create({
     borderColor: '#090f16'
   },
   swipe: {
-    paddingLeft: 5,
+    paddingLeft: 15,
     paddingRight: 5,
     paddingTop: 15,
     paddingBottom: 15,
     flexDirection: 'row'
   },
-  done: {
-    fontSize: 8,
-    fontWeight: 'bold',
-    color: '#419648'
-  },
-  notdone: {
-    fontSize: 8,
-    fontWeight: 'bold',
-    color: '#D93F3F'
-  },
-  doneimg: {
+  img: {
     width: 26,
     height: 26,
     position: 'absolute',
-    right: 15,
-    top: 21
-  },
-  notdoneimg: {
-    width: 23,
-    height: 23,
-    position: 'absolute',
-    right: 16,
-    top: 22
+    right: 20,
+    top: 16
   }
 });
 
