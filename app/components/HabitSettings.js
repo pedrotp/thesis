0b3af65119d373cdb436d41aaed9da7bd218c0d7
Derek Huang <derekh919@gmail.com>
2016-03-25 03:26:21 -0400

    (refactor) Refactors HabitSettings.js

--- app/components/HabitSettings.js
+++ app/components/HabitSettings.js
@@ -6,8 +6,10 @@ console.ignoredYellowBox = [
 ];
 
 var React = require('react-native');
+var api = require('../lib/api');
 var View = React.View;
 var Text = React.Text;
+var Alert = React.Alert;
 var TextInput = React.TextInput;
 var StyleSheet = React.StyleSheet;
 var Navigator = React.Navigator;
@@ -18,15 +20,73 @@ var Switch = React.Switch;
 var HabitSettings = React.createClass({
   getInitialState: function () {
     return {
-      date: new Date(),
-      reminder: false,
+      habit: {
+        action: this.props.habit.action,
+        frequency: this.props.habit.frequency,
+        id: this.props.habit._id,
+        createdAt: this.props.habit.createdAt,
+        updatedAt: this.props.habit.updatedAt,
+        instanceCount: this.props.habit.instanceCount,
+        instancesId: this.props.habit.instancesId,
+        streak: {
+          current: this.props.habit.streak.current,
+          max: this.props.habit.streak.max,
+          maxDate: this.props.habit.streak.maxDate
+        },
+        lastDone: this.props.habit.lastDone,
+        reminder: {
+          set: this.props.habit.reminder.set,
+          time: this.props.habit.reminder.time
+        }
+      }
     };
   },
-
-  onDateChange: function (date) {
-    this.setState({date: date})
+  componentDidMount: function () {
+    // console.log('HAB:', this.state.habit);
   },
-
+  onDateChange: function (date) {
+    this.setState({ habit: { reminder: { ...this.state.habit.reminder, time: date }}});
+  },
+  onTextChange: function (text) {
+    this.setState({ habit: { ...this.state.habit, action: text }})
+  },
+  onReminderChange: function (bool) {
+    this.setState({ habit: { reminder: { ...this.state.habit.reminder, set: bool }}});
+  },
+  onEndTextEdit: function () {
+    // console.log('YOLO?', this.state.habit);
+  },
+  deleteHabit: function (habitId) {
+    var _this = this;
+    // TODO: refactor server call to api library
+    // Remove from server
+    fetch(process.env.SERVER + '/habits/' + this.props.profile.email + '/' + habitId, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + this.props.token.idToken
+      }
+    })
+    .then(api.handleErrors)
+    // Get updated habit list
+    .then(function (response) {
+      // _this.getHabits();
+      Alert.alert(
+        'Habit Deleted',
+        null,
+        [
+          {
+            text: 'Ok',
+            onPress: function () {
+              _this.props.navigator.push({ id: 'Habits' });
+            }
+          }
+        ]
+      );
+    })
+    .catch(function (err) {
+      console.warn(err);
+    });
+  },
   render: function () {
     return (
       <View style={{ flex: 1 }}>
@@ -42,30 +102,35 @@ var HabitSettings = React.createClass({
       </View>
     );
   },
-
   renderScene: function (route, navigator) {
     var _this = this;
-    if (this.state.reminder) {
+    if (this.state.habit.reminder.set) {
       return (
         <View style={styles.container}>
-          <Text style={styles.heading}>
-            {this.props.habit.action}
-          </Text>
+          <TextInput
+            style={styles.heading}
+            defaultValue={this.props.habit.action}
+            onChangeText={this.onTextChange}
+          />
           <View style={{ flexDirection: 'row', marginTop: 60 }}>
-            <Text style={{fontSize: 22}}>Reminder</Text>
+            <Text style={{fontSize: 22}}>
+              Reminder
+            </Text>
             <Switch
-              onValueChange={function (date) { _this.setState({reminder: date}); }}
+              onValueChange={this.onReminderChange}
               style={{left: 190, marginBottom: 30}}
-              value={this.state.reminder}
+              value={this.state.habit.reminder.set}
             />
           </View>
             <DatePickerIOS
-              date={this.state.date}
+              date={new Date()}
               mode="time"
               minuteInterval={5}
               onDateChange={this.onDateChange}
             />
-          <TouchableOpacity style={styles.button} onPress={function () {_this.props.deleteHabit(_this.props.habit._id)}}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={function () { _this.deleteHabit(_this.state.habit.id) }}>
             <Text style={{color: '#FFFFFF'}}>
               Delete Habit
             </Text>
@@ -75,18 +140,25 @@ var HabitSettings = React.createClass({
     } else {
       return (
         <View style={styles.container}>
-          <Text style={styles.heading}>
-            {this.props.habit.action}
-          </Text>
+          <TextInput
+            style={styles.heading}
+            defaultValue={this.props.habit.action}
+            onChangeText={this.onTextChange}
+            onEndEditing={this.onEndTextEdit}
+          />
           <View style={{ flexDirection: 'row', marginTop: 60, marginBottom: 216 }}>
-            <Text style={{fontSize: 22}}>Reminder</Text>
+            <Text style={{fontSize: 22}}>
+              Reminder
+            </Text>
             <Switch
-              onValueChange={function (date) { _this.setState({reminder: date}); }}
+              onValueChange={this.onReminderChange}
               style={{left: 190, marginBottom: 30}}
-              value={this.state.reminder}
+              value={this.state.habit.reminder.set}
             />
           </View>
-          <TouchableOpacity style={styles.button} onPress={function () {_this.props.deleteHabit(_this.props.habit._id)}}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={function () { _this.deleteHabit(_this.state.habit.id) }}>
             <Text style={{color: '#FFFFFF'}}>
               Delete Habit
             </Text>
@@ -98,10 +170,11 @@ var HabitSettings = React.createClass({
 });
 
 var NavigationBarRouteMapper = {
-  LeftButton(route, navigator, index, navState) {
+  LeftButton: function (route, navigator, index, navState) {
     return (
-      <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}
-          onPress={function () {navigator.parentNavigator.pop()}}>
+      <TouchableOpacity
+        style={{flex: 1, justifyContent: 'center'}}
+        onPress={function () { navigator.parentNavigator.pop(); }}>
         <Text style={{color: 'white', margin: 10}}>
           Back
         </Text>
@@ -109,11 +182,11 @@ var NavigationBarRouteMapper = {
     );
   },
 
-  RightButton(route, navigator, index, navState) {
+  RightButton: function (route, navigator, index, navState) {
     return null;
   },
 
-  Title(route, navigator, index, navState) {
+  Title: function (route, navigator, index, navState) {
     return (
       <TouchableOpacity style={{flex: 1, justifyContent: 'center'}}>
         <Text style={{color: 'white', margin: 10, fontSize: 16}}>
@@ -128,11 +201,12 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 20,
-    top: 100,
+    top: 120,
   },
   heading: {
-    fontSize: 34,
-    alignSelf: 'center',
+    height: 40,
+    textAlign: 'center',
+    fontSize: 34
   },
   button: {
     height: 30,
