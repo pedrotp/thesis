d58e7d0083ce0479e0bb5347beede29f82c2ca63
Justin Bitely <jbitely@gmail.com>
2016-03-25 22:24:32 -0400

    (feat) Adds TabBarIOS component for bottom navigation
    * Imports react-native-vector-icons node module and pod

--- index.ios.js
+++ index.ios.js
@@ -1,9 +1,15 @@
 'use strict';
 var React = require('react-native')
 var AppRegistry = React.AppRegistry;
+var TabBarIOS = React.TabBarIOS;
+var View = React.View;
+var Text = React.Text;
+var Image = React.Image;
+var Icon = require('react-native-vector-icons/FontAwesome');
 var AppContainer = require('./app/containers/AppContainer');
+var CreateContainer = require('./app/containers/CreateContainer')
 
-var localServer = false;
+var localServer = true;
 
 if (localServer === true) {
   process.env.SERVER = 'http://localhost:3000'
@@ -11,6 +17,47 @@ if (localServer === true) {
   process.env.SERVER = 'http://better-habits.herokuapp.com'
 }
 
+var TabContainer = React.createClass({
+  getInitialState: function () {
+    return {
+      selectedTab: 'inbox',
+    }
+  },
+  render: function () {
+    var _this = this;
+    return (
+      <TabBarIOS selectedTab={this.state.selectedTab}>
+        <Icon.TabBarItemIOS
+          selected={this.state.selectedTab === 'inbox'}
+          title='Inbox'
+          iconName='inbox'
+          onPress={function () {
+            _this.setState({
+              selectedTab: 'inbox',
+            });
+          }}
+        >
+          <AppContainer />
+        </Icon.TabBarItemIOS>
+        <Icon.TabBarItemIOS
+          selected={this.state.selectedTab === 'profile'}
+          title='Profile'
+          iconName='user'
+          onPress={function () {
+            _this.setState({
+              selectedTab: 'profile',
+            });
+          }}
+        >
+          <View>
+            <Text>Profile View Placeholder</Text>
+          </View>
+        </Icon.TabBarItemIOS>
+      </TabBarIOS>
+    )
+  }
+});
+
 AppRegistry.registerComponent('thesis', function () {
-  return AppContainer;
+  return TabContainer;
 });
