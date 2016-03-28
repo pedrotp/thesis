8d0098e306ea8d10f38b85b9b07f67fa0a66f9e6
Derek Huang <derekh919@gmail.com>
2016-03-28 00:31:32 -0400

    (feat) Adds 'Recently Earned Badges' to Profile.js

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -2,13 +2,25 @@ var React = require('react-native');
 var ProgressBar = require('react-native-progress-bar');
 var View = React.View;
 var Text = React.Text;
+var Image = React.Image;
+var ListView = React.ListView;
 var StyleSheet = React.StyleSheet;
 var TouchableOpacity = React.TouchableOpacity;
 
 var Profile = React.createClass({
   getInitialState: function () {
+    var ds = new ListView.DataSource({
+      rowHasChanged: function (row1, row2) {
+        return row1 !== row2;
+      }
+    });
+    var mocks = [];
+    for (var i = 0; i < 4; i++) {
+      mocks.push('Image');
+    }
     return {
-      progress: 0.75
+      progress: 0.75,
+      dataSource: ds.cloneWithRows(mocks)
     }
   },
   componentDidMount: function () {
@@ -17,6 +29,23 @@ var Profile = React.createClass({
     // so upon component mount, add and subtract trivial amount
     this.setState({ progress: this.state.progress + 0.00001 });
     this.setState({ progress: this.state.progress - 0.00001 });
+  },
+  renderRow: function (rowData, sectionID, rowID) {
+    return (
+      <TouchableOpacity onPress={
+          function () {
+            console.log('rowData:', rowData);
+            console.log('sectionID:', sectionID);
+            console.log('rowID:', rowID);
+          }
+        }>
+        <View style={styles.recentBadges}>
+          <Text>
+            {rowData}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
   },
   render: function () {
     return (
@@ -28,12 +57,31 @@ var Profile = React.createClass({
             </Text>
           </View>
         </TouchableOpacity>
-        <ProgressBar
-          fillStyle={styles.progressFill}
-          backgroundStyle={styles.progress}
-          style={{marginTop: 10, width: 300, height: 10}}
-          progress={this.state.progress}
-        />
+        <View>
+          <Text>
+            Recently Earned Badges
+          </Text>
+          <ListView
+            contentContainerStyle={styles.recentBadges}
+            dataSource={this.state.dataSource}
+            initialListSize={4}
+            pageSize={4}
+            renderRow={this.renderRow}
+            scrollEnabled={false}
+            automaticallyAdjustContentInsets={false}
+          />
+        </View>
+        <View>
+          <Text style={styles.headers}>
+            1 more completion for Awesome Badge
+          </Text>
+          <ProgressBar
+            fillStyle={styles.progressFill}
+            backgroundStyle={styles.progress}
+            style={{marginTop: 10, width: 300, height: 10}}
+            progress={this.state.progress}
+            />
+        </View>
       </View>
     );
   },
@@ -45,17 +93,26 @@ var styles = StyleSheet.create({
     alignSelf: 'center',
     alignItems: 'center',
     justifyContent: 'center',
-    width: 90,
-    height: 90,
+    width: 70,
+    height: 70,
     backgroundColor: '#EEE',
     borderRadius: 100/2,
     borderColor: '#FFF',
-    borderWidth: 1
+    borderWidth: 1,
+    marginBottom: 80
   },
   avatarText: {
-    fontSize: 12,
+    fontSize: 10,
     width: 60,
     textAlign: 'center'
+  },
+  headers: {
+    alignSelf: 'center',
+  },
+  list: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
   },
   progressFill: {
     backgroundColor: '#6399DC',
@@ -63,8 +120,18 @@ var styles = StyleSheet.create({
   },
   progress: {
     backgroundColor: '#aaa',
-    borderRadius: 4,
+    borderRadius: 7,
     alignSelf: 'center'
+  },
+  recentBadges: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#aaa',
+    borderRadius: 4,
+    width: 50,
+    height: 50,
+    padding: 1,
+    margin: 1,
   }
 });
 
