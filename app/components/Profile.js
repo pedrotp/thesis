4968ff5a971dce29e1daa4916df8d0d677657d6f
Derek Huang <derekh919@gmail.com>
2016-03-27 21:41:15 -0400

    (feat) Adds progress bar to Profile.js

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,20 +1,47 @@
 var React = require('react-native');
+var ProgressBar = require('react-native-progress-bar');
 var View = React.View;
 var Text = React.Text;
 var StyleSheet = React.StyleSheet;
 var TouchableOpacity = React.TouchableOpacity;
 
 var Profile = React.createClass({
+  getInitialState: function () {
+    return {
+      progress: 0.75
+    }
+  },
+  componentDidMount: function() {
+
+    // Progress bar doesn't appear filled unless it's changed
+    // so upon component mount, add and subtract trivial amount
+    this.setState({ progress: this.state.progress + 0.00001 });
+    this.setState({ progress: this.state.progress - 0.00001 });
+  },
   render: function () {
+    // setTimeout((function () {
+    //   this.setState({ progress: this.state.progress + 0.05 });
+    // }).bind(this), 1000);
+
     return (
       <View>
         <Text>Here is some profile text</Text>
+        <ProgressBar
+          fillStyle={{}}
+          backgroundStyle={{backgroundColor: '#aaa', borderRadius: 2}}
+          style={{marginTop: 10, width: 300}}
+          progress={this.state.progress}
+        />
       </View>
     );
   },
 });
 
 var styles = StyleSheet.create({
+  fill: {
+    color: '#000000',
+    backgroundColor: '#000000'
+  }
 });
 
 module.exports = Profile;
