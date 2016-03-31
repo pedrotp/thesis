bea3422e1567dace20eebc8ddee9447c4a71dc7a
Justin Bitely <jbitely@gmail.com>
2016-03-30 23:50:50 -0400

    (style) Changes font in habit history view to Avenir

--- app/components/InstanceHistory.js
+++ app/components/InstanceHistory.js
@@ -42,12 +42,10 @@ var InstanceHistory = React.createClass({
       return (
         <View style={styles.row} >
           <Text style={styles.date} >{ moment(rowData.ISOString).format('MMMM Do YYYY') }</Text>
-          <TouchableOpacity style={ styles.touch }>
-            <Image
-                source={ {uri: 'http://better-habits.herokuapp.com/assets/done_green.png'} }
-                style={ styles.img }
-            />
-          </TouchableOpacity>
+          <Image
+              source={ {uri: 'http://better-habits.herokuapp.com/assets/done_green.png'} }
+              style={ styles.img }
+          />
         </View>
       )
     }
@@ -138,22 +136,22 @@ var styles = StyleSheet.create({
   header: {
     padding: 10,
     margin: 5,
-    fontSize: 20,
+    fontSize: 34,
+    fontFamily: 'Avenir',
     textAlign: 'center'
   },
   date: {
     marginTop: 10,
     marginBottom: 10,
-    fontSize: 18
-  },
-  touch: {
-    position: 'absolute',
-    right: 20,
-    top: 16
+    fontSize: 18,
+    fontFamily: 'Avenir',
   },
   img: {
+    position: 'absolute',
     width: 26,
     height: 26,
+    right: 20,
+    top: 16
   }
 });
 
