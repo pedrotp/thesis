973f3a5a0a71ffe3c263d0342b2af2f091f82402
Derek Huang <derekh919@gmail.com>
2016-03-31 02:39:53 -0400

    (refactor) Changes a few badges for presentation

--- app/lib/badges.js
+++ app/lib/badges.js
@@ -10,13 +10,13 @@ var badges = {
   },
   'Better Already': {
     toastText: 'You\'re Better Already!',
-    uri: 'https://better-habits.herokuapp.com/assets/Badges/Bungee.png',
+    uri: 'https://better-habits.herokuapp.com/assets/Badges/Picnic.png',
     check: function (habit) {
       return habit.instanceCount === 1;
     }
   },
   'Top of the World': {
-    toastText: 'You got your first perfect day! You\'re on Top of the World!',
+    toastText: 'Your first perfect day! You\'re on Top of the World!',
     uri: 'https://better-habits.herokuapp.com/assets/Badges/Mountain_Top.png',
     check: function (habit, store) {
       if (store.length > 2) {
@@ -31,31 +31,31 @@ var badges = {
     }
   },
   'Gone Streaking': {
-    toastText: 'You\'re On a Roll!',
+    toastText: 'You\'re on fire!',
     uri: 'https://better-habits.herokuapp.com/assets/Badges/Bon_Voyage.png',
     check: function (habit) {
       return habit.streak.current === 5;
     }
   },
   'On a Roll': {
-    toastText: 'Ten in a row! You\'re amazing!',
+    toastText: 'Ten in a row! You\'re on a roll!',
     uri: 'https://better-habits.herokuapp.com/assets/Badges/Skateboard.png',
     check: function (habit) {
       return habit.streak.current === 10;
     }
   },
   'On Point': {
-    toastText: 'Fifteen in a row? You\'re on another level!',
+    toastText: 'Fifteen in a row? You\'re On Point!',
     uri: 'https://better-habits.herokuapp.com/assets/Badges/Archery.png',
     check: function (habit) {
       return habit.streak.current === 15;
     }
   },
   'Soaring': {
-    toastText: 'You earned the Twenty Streak badge!',
+    toastText: 'Twenty in a row? You\'re on another level!',
     uri: 'https://better-habits.herokuapp.com/assets/Badges/Hot_Air_Balloon.png',
-    check: function () {
-      return false;
+    check: function (habit) {
+      return habit.streak.current === 20;
     }
   },
   'Dangerous': {
@@ -72,9 +72,9 @@ var badges = {
       return false;
     }
   },
-  'Music to my Ears': {
+  'Leap of Faith': {
     toastText: 'You earned the Thirty-Five Streak badge!',
-    uri: 'https://better-habits.herokuapp.com/assets/Badges/Guitar.png',
+    uri: 'https://better-habits.herokuapp.com/assets/Badges/Bungee.png',
     check: function () {
       return false;
     }
